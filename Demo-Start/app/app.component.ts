e1d1194d311b4ec1209a747388dcf5721c3d4fc6
Deborah Kurata <deborahk@insteptech.com>
2016-10-14 17:34:00 -0700

    Make the form larger.

--- Demo-Start/app/app.component.ts
+++ Demo-Start/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2: Reactive Forms</h1>
-        <my-signup></my-signup>
+        <div class="container">
+          <h1>Angular 2: Reactive Forms</h1>
+          <my-signup></my-signup>
+        </div>
     `
 })
 export class AppComponent { }
