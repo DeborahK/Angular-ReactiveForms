9e2ec262e0f012d722fb27549c0d991fff990670
Deborah Kurata <deborahk@insteptech.com>
2016-10-26 17:51:27 -0700

    After module 4

--- Demo-Final/app/customers/customer.component.ts
+++ Demo-Final/app/customers/customer.component.ts
@@ -1,7 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
-import { Customer } from './customer'
+import { Customer } from './customer';
+
+function ratingRange(min: number, max: number): ValidatorFn {
+    return (c: AbstractControl): { [key: string]: boolean } | null => {
+        if (c.value && (isNaN(c.value) || c.value < min || c.value > max)) {
+            return { 'range': true };
+        }
+        return null;
+    };
+}
+
+function emailMatcher(c: AbstractControl) {
+    let emailControl = c.get('email');
+    let confirmControl = c.get('confirmEmail');
+
+    // Don't kick in until user touches both fields   
+    if (emailControl.pristine || confirmControl.pristine) {
+      return null;
+    }
+
+    if (emailControl.value === confirmControl.value) {
+        return null;
+    } else {
+        //emailControl.setErrors({ 'match': true });
+        //confirmControl.setErrors({ 'match': true });
+        return { 'match': true };
+    }
+}
 
 @Component({
     selector: 'my-signup',
@@ -9,20 +36,36 @@ import { Customer } from './customer'
 })
 export class CustomerComponent implements OnInit {
     customerForm: FormGroup;
-    customer: Customer= new Customer();
+    customer: Customer = new Customer();
 
     constructor(private fb: FormBuilder) { }
 
     ngOnInit(): void {
         this.customerForm = this.fb.group({
-            firstName: '',
-            lastName: '',
-            email: '',
-            sendCatalog: true
+            firstName: [null, [Validators.required, Validators.minLength(3)]],
+            lastName: [null, [Validators.required, Validators.maxLength(50)]],
+            emailGroup: this.fb.group({
+                email: [null, [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]],
+                confirmEmail: [null, Validators.required]
+            }, { validator: emailMatcher }),
+            phone: null,
+            notification: 'email',
+            sendCatalog: true,
+            rating: [null, ratingRange(1, 5)]
         });
     }
 
-    save() {
-        console.log("Saved: ");
+    save(): void {
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value);
     }
- }
+
+    setNotification(notifyVia: string): void {
+        let phoneControl = this.customerForm.get('phone');
+        if (notifyVia === 'text') {
+            phoneControl.setValidators(Validators.required);
+        } else {
+            phoneControl.clearValidators();
+        }
+        phoneControl.updateValueAndValidity();
+    }
+}
