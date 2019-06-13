af872d5027575ccd25f1c08e2fdab59175aee047
mbartongawryn <mbartongawryn@HQCO-MBARTONGAW>
2019-06-13 11:51:29 -0600

    ng update to 8.0

--- Demo-Start/src/polyfills.ts
+++ Demo-Start/src/polyfills.ts
@@ -73,7 +73,7 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-import 'core-js/es7/reflect'; // Required for Stackblitz
+import 'core-js/es/reflect'; // Required for Stackblitz
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
