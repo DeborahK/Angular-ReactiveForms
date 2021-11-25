8bdf509797cb9754ead45ceebd13d77a71469114
Steve <Steven.Kan@Lancom.tech>
2021-11-26 10:50:08 +1300

    Create route for Home page and Sign up page

--- demo-final-v12/src/app/app.component.ts
+++ demo-final-v12/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo';
+  title = 'Acne Product Management XD';
 }
--- demo-final-v12/src/app/app.module.ts
+++ demo-final-v12/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customers/customer.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomerComponent
+    CustomerComponent,
+    WelcomeComponent
   ],
   imports: [
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'signup', component: CustomerComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    
   ],
   bootstrap: [AppComponent]
 })
