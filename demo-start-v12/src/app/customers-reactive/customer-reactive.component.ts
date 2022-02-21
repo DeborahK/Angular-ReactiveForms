934f2599a72dcfea6eac47b5ba33346ea3f4867b
Cc <1625191507@qq.com>
2022-02-22 00:32:44 +0800

    “[ReactiveForm]:formBuilder”

--- demo-start-v12/src/app/customers-reactive/customer-reactive.component.ts
+++ demo-start-v12/src/app/customers-reactive/customer-reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 
 import { Customer } from './customer';
@@ -13,15 +13,23 @@ export class CustomerReactiveComponent implements OnInit {
   customer = new Customer();
   customerForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.customerForm = new FormGroup({
+    this.customerForm = this.fb.group(
+      {
+        firstName: '', // 写法 1
+        lastName: {value: 'n/a', disabled: true},// 写法 2
+        email: [{value: '', disabled: false}],// 写法 3
+        sendCataLog: [true]               // 写法 3
+      }
+    )
+    /* this.customerForm = new FormGroup({
       firstName: new FormControl(),
       lastName: new FormControl(),
       email: new FormControl(),
       sendCataLog: new FormControl(true)
-    });
+    }); */
   }
 
   save(): void {
